2dc91725a721cfd06a37a0f4a3edb39331163e33
Ahmed Salim AL-Abri <49365123+alabri1998@users.noreply.github.com>
2023-05-16 16:16:56 +0400

    Add files via upload

---
+++ week2_day3_challenge/week2_day3_challenge.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 13:48:28 2023
+
+@author: user
+"""
+
+print("    *   *   ")
+print("   **   **  ")
+print("  ***   *** ")
+print(" ****   **** ")
+print("*****---*****")
+print("*****---*****")
+print(" ****   **** ")
+print("  ***   *** ")
+print("   **   **  ")
+print("    *   *   ")
